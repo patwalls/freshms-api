bf58c119d526775ece5e6af88aac49b84e1a6574
Pat Walls <patrickswalls@gmail.com>
2016-08-11 20:35:37 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreshmsApi::Application.config.secret_key_base = '6da528c18ed8fa72663264d08f69ef725fe9ebd505505ab51105e8027bf3d99216e8aa966d95948d183aee42f42edafc60939ec5bf743a1eabbdaf5455ac584c'
